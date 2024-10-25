d3edecfa8139389a6436618ce3482a0ba3ff725b
melendez <melenava@gmail.com>
2024-10-25 17:19:04 -0600

    Implementacion de AccountBalance

--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -2,28 +2,34 @@
 import WalletBar from "@/components/features/WalletBar";
 
  import BlockNumber from "@/components/features/BlockNumber";
-// import AccountBalance from "@/components/features/AccountBalance";
+ import AccountBalance from "@/components/features/AccountBalance";
 // import ReadContract from "@/components/features/ReadContract";
 // import WriteContract from "@/components/features/WriteContract";
 
 import Footer from "@/components/ui/Footer";
-
+import { useAccount } from "@starknet-react/core";
 export default function Home() {
+
+  const {address} = useAccount();
+
   return (
     <div className="flex flex-col min-h-screen">
       <main className="flex-grow flex flex-col items-center justify-center gap-8">
         <h1 className="text-3xl font-bold text-center mb-4">Starknet Bootcamp: Front End</h1>
         <WalletBar />
-        {/* <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full max-w-4xl"> */}
+        {address && (
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full max-w-4xl"> 
           {/* Step 1: Read Data - Display block number */}
           <BlockNumber /> 
           {/* Step 2: Read Balance - Display account balance */}
-          {/* <AccountBalance /> */}
+         <AccountBalance />
           {/* Step 3: Read Contract - Fetch data from deployed smart contract */}
           {/* <ReadContract /> */}
           {/* Step 4: Write Contract - Update status of smart contract */}
           {/* <WriteContract /> */}
-        {/* </div> */}
+        </div> 
+        )}
+        
       </main>
       <Footer />
     </div>
